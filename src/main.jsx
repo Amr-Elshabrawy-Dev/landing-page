b520831518f443e252bcabba30d8c7deee24d680
AmrElshabrawy <amrelshabrawy.dev@gmail.com>
2025-01-25 12:41:55 +0200

    Add initial project structure with HTML, CSS, and React components

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import App from "./App.jsx";
+import Home from "./pages/Home/Home.jsx";
+import Login from "./pages/Login/Login";
+import StartNow from "./pages/StartNow/StartNow.jsx";
+import NotFound from "./pages/NotFound/NotFound.jsx";
+import "./normalize.min.css";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route element={<App />}>
+        <Route index element={<Home />} />
+        <Route path="startNow" element={<StartNow />} />
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<NotFound/>} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
