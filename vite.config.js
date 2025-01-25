91416a9010bf66b59e4670ce68ed1d0d25a9983d
AmrElshabrawy <amrelshabrawy.dev@gmail.com>
2025-01-25 12:40:44 +0200

    Add configuration files for Prettier, ESLint, and Vite

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+
+// https://vite.dev/config/
+export default defineConfig({
+  build: {
+    sourcemap: true,
+  },
+  server: {
+    open: true,
+  },
+  plugins: [
+    react(),
+    svgr(),
+    ViteImageOptimizer({
+      webp: {
+        quality: 70,
+      },
+      cache: true,
+      cacheLocation: "node_modules/.cache/vite-plugin-image-optimizer",
+    }),
+  ],
+});
