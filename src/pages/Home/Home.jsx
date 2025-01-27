4752d55660fd2438e3d4093bcb18e9cf023ddc65
AmrElshabrawy <amrelshabrawy.dev@gmail.com>
2025-01-27 08:59:49 +0200

    Update CSS for responsive height and clean up import formatting in Home component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,11 @@
-import BackgroundAnimation from '../../components/BackgroundAnimation/BackgroundAnimation';
-import CardProducts from '../../components/CardProducts/CardProducts';
-import Companies from '../../components/Companies/Companies';
-import ContactForm from '../../components/ContactForm/ContactForm';
-import Hero from '../../components/Hero/Hero';
-import OurCustomers from '../../components/OurCustomers/OurCustomers';
-import SellStunning from '../../components/SellStunning/SellStunning';
-import './Home.css';
-
+import BackgroundAnimation from "../../components/BackgroundAnimation/BackgroundAnimation";
+import CardProducts from "../../components/CardProducts/CardProducts";
+import Companies from "../../components/Companies/Companies";
+import ContactForm from "../../components/ContactForm/ContactForm";
+import Hero from "../../components/Hero/Hero";
+import OurCustomers from "../../components/OurCustomers/OurCustomers";
+import SellStunning from "../../components/SellStunning/SellStunning";
+import "./Home.css";
 const Home = () => {
   return (
     <div className="home">
@@ -16,9 +15,8 @@ const Home = () => {
       <SellStunning />
       <CardProducts />
       <OurCustomers />
-      <ContactForm />
+      <ContactForm/>
     </div>
   );
 };
-
 export default Home;
